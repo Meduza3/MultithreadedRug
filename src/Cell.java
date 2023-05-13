64a49b337917cee033d2d309a0df52331e7a1b99
Meduza3 <jellyfish200325@gmail.com>
2023-05-13 19:00:41 +0200

    Added averaging the colour of neighbours

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,4 @@
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 public class Cell extends Rectangle implements Runnable {
@@ -6,6 +7,7 @@ public class Cell extends Rectangle implements Runnable {
     private double width;
     private double height;
     private Color color;
+    private String id;
     public RandomGenerator rng = new RandomGenerator();
     public Cell(double x, double y, double width, double height) {
         super(x, y, width, height);
@@ -19,14 +21,16 @@ public class Cell extends Rectangle implements Runnable {
         this.y = y;
     }
 
+
     @Override
     public void run() {
         while(true){
             System.out.println("Start: " + Thread.currentThread().getName());
             if( rng.nextDouble() < Controller.getProbability()){
-                Color col = new Color(rng.nextDouble(), rng.nextDouble(), rng.nextDouble(), 1);
+                System.out.println("Changing color");
+                Color col = getAverageColor();
                 setFill(col);
-                //setStroke(col);
+                setStroke(col);
             }
             try {
                 Thread.sleep((long) (rng.nextInt(Controller.getSpeed()) + Controller.getSpeed()/2));
@@ -35,5 +39,43 @@ public class Cell extends Rectangle implements Runnable {
             }
             System.out.println("End: " + Thread.currentThread().getName());
         }
+    }
+
+    private Cell getCell(int x, int y, Pane pane){
+        return (Cell) pane.lookup("#"+Integer.toString(x) + "," + Integer.toString(y));
+    }
+
+    private Color getAverageColor(){
+        Cell left = getCell((int) (x/width) - 1, (int) (y/height), (Pane) getParent());
+        Cell right = getCell((int) (x/width) + 1, (int) (y/height), (Pane) getParent());
+        Cell up = getCell((int) (x/width), (int) (y/height) - 1, (Pane) getParent());
+        Cell down = getCell((int) (x/width), (int) (y/height) + 1, (Pane) getParent());
+
+        if (left == null) {
+            left = getCell(Controller.getn() - 1, (int) (y/height), (Pane) getParent());
+        }
+        if (up == null) {
+            up = getCell((int) (x/width),Controller.getm() - 1, (Pane) getParent());
+        }
+        if (right == null) {
+            right = getCell(0, (int) (y/height), (Pane) getParent());
+        }
+        if (down == null) {
+            down = getCell((int) (x/width), 0, (Pane) getParent());
+        }
+
+        System.out.println(left.id);
+        System.out.println(right.id);
+        System.out.println(up.id);
+        System.out.println(down.id);
+
+        Color cl = (Color) left.getFill();
+        Color cr = (Color) right.getFill();
+        Color cu = (Color) up.getFill();
+        Color cd = (Color) down.getFill();
+        double red = (cl.getRed() + cr.getRed() + cu.getRed() + cd.getRed())/4;
+        double green = (cl.getGreen() + cr.getGreen() + cu.getGreen() + cd.getGreen())/4;
+        double blue = (cl.getBlue() + cr.getBlue() + cu.getBlue() + cd.getBlue())/4;
+        return new Color(red, green, blue, 1);
     }
 }
--- src/Controller.java
+++ src/Controller.java
@@ -12,8 +12,10 @@ public class Controller implements Initializable {
     private double cellHeight;
     private static int speed = 1000;
     private static double probability = 0.2;
+    static int n;
+    static int m;
     @FXML
-    private Pane pane;
+    public Pane pane;
 
     public RandomGenerator rng = new RandomGenerator();
 
@@ -21,17 +23,18 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Initializing");
-        int n = 4; //Number of cells in a row
-        int m = 2; //Number of cells in a column
+        n = 80; //Number of cells in a row
+        m = 40; //Number of cells in a column
         cellwidth = (double) 1000 / n;
         cellHeight = (double) 500 / m;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 Cell cell = new Cell(i * cellwidth, j * cellHeight, cellwidth, cellHeight);
+                cell.setId(Integer.toString(i) + "," + Integer.toString(j));
                 Thread thread = new Thread(cell, "(" + Integer.toString(i) + ", " + Integer.toString(j) + ")");
                 Color col = new Color(rng.nextDouble(), rng.nextDouble(), rng.nextDouble(), 1);
                 cell.setFill(col);
-                //cell.setStroke(col);
+                cell.setStroke(col);
                 pane.getChildren().add(cell);
                 thread.start();
                 System.out.println("Added a cell at " + i * cellwidth + ", " + j * cellHeight);
@@ -46,5 +49,13 @@ public class Controller implements Initializable {
     }
     static public int getSpeed(){
         return speed;
+    }
+
+    public static int getn(){
+        return n;
+    }
+
+    public static int getm(){
+        return m;
     }
 }
